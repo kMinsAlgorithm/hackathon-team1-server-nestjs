7728658a4d0bc5bd8ec1001289174d33a5d4c0bb
k013600 <k012123600@gmail.com>
2023-08-12 19:55:30 +0900

    fix: cors 설정

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({
+    // origin: new RegExp(process.env.CORS_WHITELIST_REGEX),
+    origin: 'http://localhost:3000/',
+  });
   await app.listen(3000);
 }
 bootstrap();
