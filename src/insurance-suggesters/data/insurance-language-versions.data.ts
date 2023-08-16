31c823bc9370c5315a2e8b9dcb977b1fc7ee0a0a
k013600 <k012123600@gmail.com>
2023-08-17 00:39:28 +0900

    feature: 한 중 영 언어 버전 같이 리턴

---
+++ src/insurance-suggesters/data/insurance-language-versions.data.ts
@@ -0,0 +1,354 @@
+export const INSURANCE_LANGUAGE_OBJECT: {
+  [key: string]: { [subKey: string]: { KR: string; CN: string; EN: string } };
+} = {
+  '무배당let:click실손의료보험Ⅳ(2107)': {
+    'registration type': {
+      KR: '온라인 가입',
+      CN: '在线申请',
+      EN: 'Online Application',
+    },
+    'insurance name': {
+      KR: '무배당let:click실손의료보험Ⅳ(2107)',
+      CN: '无红利let:click实支医疗保险Ⅳ(2107)',
+      EN: 'Non-dividend let:click Medical Expense Insurance Ⅳ(2107)',
+    },
+    'insurance company': {
+      KR: '롯데 손해보험',
+      CN: '乐天保险',
+      EN: 'Lotte Insurance',
+    },
+  },
+  '(무)다이렉트실손의료비보험(23.01)': {
+    'registration type': {
+      KR: '온라인 가입',
+      CN: '在线申请',
+      EN: 'Online Application',
+    },
+    'insurance name': {
+      KR: '(무)다이렉트실손의료비보험(23.01)',
+      CN: '(无)直接医疗费用保险(23.01)',
+      EN: '(No-cost) Direct Medical Expense Insurance (23.01)',
+    },
+    'insurance company': {
+      KR: 'MG 손해보험',
+      CN: 'MG 保险',
+      EN: 'MG Insurance',
+    },
+  },
+  '무배당교보실손의료비보험(갱신형)Ⅲ_2107': {
+    'registration type': {
+      KR: '설계사 상담',
+      CN: '规划师咨询',
+      EN: 'Agent Consultation',
+    },
+    'insurance name': {
+      KR: '무배당교보실손의료비보험(갱신형)Ⅲ_2107',
+      CN: '无红利教保实支医疗费用保险(更新型)Ⅲ_2107',
+      EN: 'Non-dividend Kyobo Direct Medical Expense Insurance (Renewable) Ⅲ_2107',
+    },
+    'insurance company': {
+      KR: '교보생명',
+      CN: '教保人寿保险',
+      EN: 'Kyobo Life Insurance',
+    },
+  },
+  'New안심케어NH실손의료비보험(갱신형,무배당)': {
+    'registration type': { KR: '', CN: '', EN: '' },
+    'insurance name': {
+      KR: 'New안심케어NH실손의료비보험(갱신형,무배당)',
+      CN: 'New安心护理NH实支医疗费用保险(更新型,无红利)',
+      EN: 'New PeaceCare NH Direct Medical Expense Insurance (Renewable, Non-dividend)',
+    },
+    'insurance company': {
+      KR: '농협생명',
+      CN: '农协人寿保险',
+      EN: 'NongHyup Life Insurance',
+    },
+  },
+  '한화생명기본형급여e실손의료비보장보험(갱신형)무배당': {
+    'registration type': {
+      KR: '온라인 가입',
+      CN: '在线申请',
+      EN: 'Online Application',
+    },
+    'insurance name': {
+      KR: '한화 생명 기본형 급여e 실손 의료비 보장 보험(갱신형)무배당',
+      CN: '韩华人寿基本型工资e实支医疗费用保险(更新型)无红利',
+      EN: 'Hanwha Life Basic Wage eDirect Medical Expense Assurance (Renewable) Non-dividend',
+    },
+    'insurance company': {
+      KR: '한화생명',
+      CN: '韩华人寿保险',
+      EN: 'Hanwha Life Insurance',
+    },
+  },
+  '(무)다이렉트실손의료비보험2301(CM)': {
+    'registration type': {
+      KR: '온라인 가입',
+      CN: '在线申请',
+      EN: 'Online Application',
+    },
+    'insurance name': {
+      KR: '(무)다이렉트실손의료비보험2301(CM)',
+      CN: '(无)直接医疗费用保险2301(CM)',
+      EN: '(No-cost) Direct Medical Expense Insurance 2301(CM)',
+    },
+    'insurance company': {
+      KR: 'DB 손해보험',
+      CN: 'DB 保险',
+      EN: 'DB Insurance',
+    },
+  },
+  '(무)헤아림다이렉트실손의료비보험2301': {
+    'registration type': {
+      KR: '온라인 가입',
+      CN: '在线申请',
+      EN: 'Online Application',
+    },
+    'insurance name': {
+      KR: '(무)헤아림다이렉트실손의료비보험2301',
+      CN: '(无)和林直接医疗费用保险2301',
+      EN: '(No-cost) Healim Direct Medical Expense Insurance 2301',
+    },
+    'insurance company': {
+      KR: '농협 손해보험',
+      CN: '农协保险',
+      EN: 'NongHyup Insurance',
+    },
+  },
+  '무배당DB기본형급여실손의료비보장보험(갱신형)(2211)': {
+    'registration type': {
+      KR: '설계사 상담',
+      CN: '规划师咨询',
+      EN: 'Agent Consultation',
+    },
+    'insurance name': {
+      KR: '무배당DB기본형급여실손의료비보장보험(갱신형)(2211)',
+      CN: '无红利DB基本型工资实支医疗费用保险(更新型)(2211)',
+      EN: 'Non-dividend DB Basic Wage Direct Medical Expense Assurance (Renewable)(2211)',
+    },
+    'insurance company': {
+      KR: 'DB 생명',
+      CN: 'DB 人寿保险',
+      EN: 'DB Life Insurance',
+    },
+  },
+  '무배당한화실손의료보험(갱신형)2301': {
+    'registration type': {
+      KR: '설계사 상담',
+      CN: '规划师咨询',
+      EN: 'Agent Consultation',
+    },
+    'insurance name': {
+      KR: '무배당한화실손의료보험(갱신형)2301',
+      CN: '无红利韩华实支医疗保险(更新型)2301',
+      EN: 'Non-dividend Hanwha Medical Expense Insurance (Renewable) 2301',
+    },
+    'insurance company': {
+      KR: '한화 손해보험',
+      CN: '韩华财产保险',
+      EN: 'Hanwha General Insurance',
+    },
+  },
+  '(무)현대해상다이렉트실손의료비보장보험(갱신형)(Hi2307)': {
+    'registration type': {
+      KR: '온라인 가입',
+      CN: '在线申请',
+      EN: 'Online Application',
+    },
+    'insurance name': {
+      KR: '(무)현대해상다이렉트실손의료비보장보험(갱신형)(Hi2307)',
+      CN: '(无)现代海上直接医疗费用保险(更新型)(Hi2307)',
+      EN: '(No-cost) Hyundai Marine Direct Medical Expense Assurance (Renewable)(Hi2307)',
+    },
+    'insurance company': {
+      KR: '현대해상',
+      CN: '现代财产保险',
+      EN: 'Hyundai Marine & Fire Insurance',
+    },
+  },
+  '삼성인터넷급여실손의료비보장보험(2301)(갱신형,무배당)': {
+    'registration type': {
+      KR: '온라인 가입',
+      CN: '在线申请',
+      EN: 'Online Application',
+    },
+    'insurance name': {
+      KR: '삼성인터넷급여실손의료비보장보험(2301)(갱신형,무배당)',
+      CN: '三星互联网工资实支医疗费用保险(2301)(更新型,无红利)',
+      EN: 'Samsung Internet Wage Direct Medical Expense Assurance (2301)(Renewable, Non-dividend)',
+    },
+    'insurance company': {
+      KR: '삼성화재',
+      CN: '三星财产保险',
+      EN: 'Samsung Fire & Marine Insurance',
+    },
+  },
+  '무배당수호천사급여실손의료비보장보험(갱신형)': {
+    'registration type': {
+      KR: '설계사 상담',
+      CN: '规划师咨询',
+      EN: 'Agent Consultation',
+    },
+    'insurance name': {
+      KR: '무배당수호천사급여실손의료비보장보험(갱신형)',
+      CN: '无红利守护天使工资实支医疗费用保险(更新型)',
+      EN: 'Non-dividend Guardian Angel Wage Direct Medical Expense Assurance (Renewable)',
+    },
+    'insurance company': {
+      KR: '동양생명',
+      CN: '东洋人寿保险',
+      EN: 'Dongyang Life Insurance',
+    },
+  },
+  '삼성급여실손의료비보장보험(2301)(갱신형,무배당)': {
+    'registration type': { KR: '', CN: '', EN: '' },
+    'insurance name': {
+      KR: '삼성급여실손의료비보장보험(2301)(갱신형,무배당)',
+      CN: '三星工资实支医疗费用保险(2301)(更新型,无红利)',
+      EN: 'Samsung Wage Direct Medical Expense Assurance (2301)',
+    },
+    'insurance company': {
+      KR: '삼성화재',
+      CN: '三星财产保险',
+      EN: 'Samsung Fire & Marine Insurance',
+    },
+  },
+  메리츠다이렉트실손의료비보험2304: {
+    'registration type': {
+      KR: '온라인 가입',
+      CN: '在线申请',
+      EN: 'Online Application',
+    },
+    'insurance name': {
+      KR: '메리츠다이렉트실손의료비보험2304',
+      CN: '三星直接医疗费用保险2304',
+      EN: 'Meritz Direct Medical Expense Insurance 2304',
+    },
+    'insurance company': {
+      KR: '메리츠화재',
+      CN: '三星财产保险',
+      EN: 'Meritz Fire & Marine Insurance',
+    },
+  },
+  '무배당삼성화재다이렉트실손의료비보험(2304.3)': {
+    'registration type': {
+      KR: '온라인 가입',
+      CN: '在线申请',
+      EN: 'Online Application',
+    },
+    'insurance name': {
+      KR: '무배당삼성화재다이렉트실손의료비보험(2304.3)',
+      CN: '无红利三星火灾直接医疗保险(2304.3)',
+      EN: 'Non-dividend Samsung Fire Direct Medical Expense Insurance (2304.3)',
+    },
+    'insurance company': {
+      KR: '삼성화재',
+      CN: '三星财产保险',
+      EN: 'Samsung Fire & Marine Insurance',
+    },
+  },
+  '무배당흥국생명실손의료비보험(갱신형)': {
+    'registration type': {
+      KR: '설계사 상담',
+      CN: '规划师咨询',
+      EN: 'Agent Consultation',
+    },
+    'insurance name': {
+      KR: '무배당흥국생명실손의료비보험(갱신형)',
+      CN: '无红利兴国人寿实支医疗保险(更新型)',
+      EN: 'Non-dividend Heungkuk Life Direct Medical Expense Insurance (Renewable)',
+    },
+    'insurance company': {
+      KR: '흥국생명',
+      CN: '兴国人寿保险',
+      EN: 'Heungkuk Life Insurance',
+    },
+  },
+  '(무)흥국화재다이렉트실손의료보험(23.07)': {
+    'registration type': {
+      KR: '온라인 가입',
+      CN: '在线申请',
+      EN: 'Online Application',
+    },
+    'insurance name': {
+      KR: '(무)흥국화재다이렉트실손의료보험(23.07)',
+      CN: '(无)兴国火灾直接医疗保险(23.07)',
+      EN: '(No-cost) Heungkuk Fire Direct Medical Insurance (23.07)',
+    },
+    'insurance company': {
+      KR: '흥국화재',
+      CN: '兴国财产保险',
+      EN: 'Heungkuk Fire & Marine Insurance',
+    },
+  },
+  '무배당한화 손해보험다이렉트실손의료비보장보험(23.01)': {
+    'registration type': {
+      KR: '온라인 가입',
+      CN: '在线申请',
+      EN: 'Online Application',
+    },
+    'insurance name': {
+      KR: '무배당한화 손해보험다이렉트실손의료비보장보험(23.01)',
+      CN: '无红利韩华财产保险直接实支医疗费用保险(23.01)',
+      EN: 'Non-dividend Hanwha Property Insurance Direct Medical Expense Assurance (23.01)',
+    },
+    'insurance company': {
+      KR: '한화 손해보험',
+      CN: '韩华财产保险',
+      EN: 'Hanwha General Insurance',
+    },
+  },
+  '다이렉트 간편실손의료비보험(PreExistingConditionActualCost용)2301(CM)': {
+    'registration type': {
+      KR: '온라인 가입',
+      CN: '在线申请',
+      EN: 'Online Application',
+    },
+    'insurance name': {
+      KR: '다이렉트 간편실손의료비보험(유병력자)2301(CM)',
+      CN: '直接简便实支医疗费用保险(有病史者用)2301(CM)',
+      EN: 'Direct EasyCare Medical Expense Insurance (For Pre-existing Conditions) 2301(CM)',
+    },
+    'insurance company': {
+      KR: 'DB 손해보험',
+      CN: 'DB 保险',
+      EN: 'DB Insurance',
+    },
+  },
+  '무배당 한화 손해보험 다이렉트간편가입 실손의료비보장보험(23.01)': {
+    'registration type': {
+      KR: '온라인 가입',
+      CN: '在线申请',
+      EN: 'Online Application',
+    },
+    'insurance name': {
+      KR: '무배당 한화 손해보험 다이렉트간편가입 실손의료비보장보험(23.01)',
+      CN: '无红利韩华财产保险直接简便参保实支医疗费用保险(23.01)',
+      EN: 'Non-dividend Hanwha Property Insurance Direct Easy Enrollment Direct Medical Expense Assurance (23.01)',
+    },
+    'insurance company': {
+      KR: '한화 손해보험',
+      CN: '韩华财产保险',
+      EN: 'Hanwha General Insurance',
+    },
+  },
+  '무배당 삼성화재 다이렉트 PreExistingConditionActualCost실손의료비보험(2304.5)':
+    {
+      'registration type': {
+        KR: '온라인 가입',
+        CN: '在线申请',
+        EN: 'Online Application',
+      },
+      'insurance name': {
+        KR: '무배당 삼성화재 다이렉트 유병력자 실손의료비보험(2304.5)',
+        CN: '无红利三星火灾直接参保有病史者实支医疗费用保险(2304.5)',
+        EN: 'Non-dividend Samsung Fire Direct Enrollment Pre-existing Conditions Direct Medical Expense Insurance (2304.5)',
+      },
+      'insurance company': {
+        KR: '삼성화재',
+        CN: '三星财产保险',
+        EN: 'Samsung Fire & Marine Insurance',
+      },
+    },
+};
