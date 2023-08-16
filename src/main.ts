6bf7e9376beded335d5b9df67d211fd9d8d06991
k013600 <k012123600@gmail.com>
2023-08-17 06:35:45 +0900

    fix: cors origin 웹 배포 링크 추가

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.enableCors({
     // origin: new RegExp(process.env.CORS_WHITELIST_REGEX),
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://hackathon-team1-6n5652jde-hackathon-team1-web.vercel.app',
+    ],
   });
   await app.listen(3000);
 }
