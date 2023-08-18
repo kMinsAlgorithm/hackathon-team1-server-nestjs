9ae58be48d24270ce609e3b1aa99153ad7b116a6
k013600 <k012123600@gmail.com>
2023-08-18 17:37:06 +0900

    fix : dto에 question이 날아갔네요..ㅎ

--- src/insurance-suggesters/dto/insurance-suggester.dto.ts
+++ src/insurance-suggesters/dto/insurance-suggester.dto.ts
@@ -3,5 +3,7 @@ import { IsString, MaxLength } from 'class-validator';
 export class InsuranceSuggesterDto {
   @IsString()
   @MaxLength(100, { message: '질문은 최대 100자 이내로 입력해주세요.' })
+  'question': string;
+
   'sourceLanguage': string;
 }
